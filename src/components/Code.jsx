1a6850624e70310b54f0bb88ce7f88cabfe0c36a
istokistvan <55344429+istokistvan@users.noreply.github.com>
2023-07-01 20:04:44 +0200

    fix link

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -241,7 +241,7 @@ export default function Code(props) {
                             code: code,
                             purchase_time: `2023-${resDate.day} ${resDate.hour}:${resDate.minute}`
                         }))
-                        window.open(`${window.location.href}/#/register`)
+                        window.open(`${window.location.href}#/register`)
                     }
                 })
         } else {
